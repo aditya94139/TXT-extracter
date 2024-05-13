2e200c243e57bf3aebb5374cb60cfd4c165d921f
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-05-13 12:00:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 21233086
-api_hash = "2892db755e485aaf8e55b4b70e8953f6"
-bot_token = "7109218345:AAHE_RTiW5-JcFJ36-vEkJNexxJm0cPjI_I"
-auth_users = "7107871207"
+api_id = 25586552
+api_hash = "f265cba9d76dc6ad70914accbe758f47"
+bot_token = "6938712783:AAFiijI3PSRJmMaF3hx8YOAGDjADGRNIJ8s"
+auth_users = "1368753935"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "7107871207"
+osowner_users = "1368753935"
 owner_users = [int(num) for num in osowner_users.split(",")]
